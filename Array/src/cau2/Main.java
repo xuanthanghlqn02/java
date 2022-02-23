d680259bd7ac635cf36dcd060f160959bb4c3fee
xuanthanghlqn02 <loanthang.photos@gmail.com>
2022-02-23 17:10:03 +0700

    array cau 2 update

---
+++ Array/src/cau2/Main.java
@@ -0,0 +1,34 @@
+package cau2;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    public static void createInt() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the integer you want: ");
+        int arr;
+        arr = sc.nextInt();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
