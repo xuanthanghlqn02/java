ce4d85888bdde3cd8a646d5268208cd56b43158c
Thang vu xuan <loanthang.photos@gmail.com>
2022-02-27 00:35:23 +0700

    update matrix test 01

--- Test01/src/Cau5/Main.java
+++ Test01/src/Cau5/Main.java
@@ -43,5 +43,50 @@ public class Main {
                 }
             }
         }
+
+        System.out.println("\n-------------------------------------------");
+        System.out.println("Nhập mảng thứ hai (có số hàng & số cột bằng mảng thứ nhất): ");
+        int[][] arr2 = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.println("Nhập vào phần tử tại hàng thứ: " + i + ", cột thứ: " + j);
+                arr2[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("In mảng thứ hai: ");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(arr2[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+        System.out.println("In lại mảng thứ nhất: ");
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n-------------------------------------------");
+        System.out.println("Thực hiện cộng hai mảng, kết quả: ");
+        int[][] result = new int[x][y];
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                result[i][j] = arr[i][j] + arr2[i][j];
+            }
+//            System.out.println();
+        }
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+
     }
 }
