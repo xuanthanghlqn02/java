73cb3417d6f9bccf1e9042fff10d1386011f05a3
Thang vu xuan <loanthang.photos@gmail.com>
2022-03-01 23:59:56 +0700

    update OOP

---
+++ OOP/src/Person/Person.java
@@ -0,0 +1,21 @@
+package Person;
+
+public class Person {
+    int id;
+    String name;
+    short age;
+    boolean gender;
+    String address;
+
+    public void job(){
+        System.out.println("Job: Doctor");
+    }
+
+    public void sleep(){
+        System.out.println("Sleep: At 10 P.M");
+    }
+
+    public void hobbit() {
+        System.out.println("Hobbit: singing");
+    }
+}
---
+++ OOP/src/Product/Main.java
@@ -0,0 +1,9 @@
+package Product;
+
+public class Main {
+    public static void main(String[] args) {
+        Product p1 = new Product("iPhone 13 PRM", 2, 999);
+        Product p2 = new Product("Macbook Pro 2021", 5, 2000);
+        System.out.println(p1 + "\t \t" + p2);
+    }
+}
