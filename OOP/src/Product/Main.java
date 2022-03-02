7159155fa3ced03ded7512e18dffcbe93cece503
Thang vu xuan <loanthang.photos@gmail.com>
2022-03-03 01:26:19 +0700

    update OOP exersice

--- OOP/src/Product/Main.java
+++
@@ -1,9 +0,0 @@
-package Product;
-
-public class Main {
-    public static void main(String[] args) {
-        Product p1 = new Product("iPhone 13 PRM", 2, 999);
-        Product p2 = new Product("Macbook Pro 2021", 5, 2000);
-        System.out.println(p1 + "\t \t" + p2);
-    }
-}
